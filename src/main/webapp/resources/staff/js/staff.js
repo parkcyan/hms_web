6189f049fb50c6d4869cd3ddcdffb16a56c47e80
ParkJW <pc224538@gmail.com>
2023-01-30 16:27:27 +0900

    stafflogin cookie

--- src/main/webapp/resources/staff/js/staff.js
+++ src/main/webapp/resources/staff/js/staff.js
@@ -53,6 +53,22 @@ function confirm(icon, title, text, callback) {
     }).then(callback);
 }
 
+function setCookie(name, value, exp) {
+	var date = new Date();
+	date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
+	document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
+}
+
+function getCookie(name) {
+	var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+	return value ? value[2] : null;
+};
+
+function deleteCookie(name) {
+	var date = new Date();
+	document.cookie = name + "= " + "; expires=" + date.toUTCString() + "; path=/"
+}
+
 function getAge(social_id) {
 	let year = Number(social_id.substring(0, 2));
 	if (year < 22) year += 2000;
